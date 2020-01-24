HaProc updated to allow LDT parts in wrong order Created a more general buffer to read use HaProcHKBuild solution found Adapted CUCtoUTC_DT for SWIS

Code/HaProc.py
@@ -6,8 +6,6 @@ binaries of the images and HK
 
 @author: ucasbwh
 
-To-Do:
-Adapt for writing HK
 """
 
 import numpy as np
@@ -27,6 +25,7 @@ logger = logging.getLogger(__name__)
 HaImageProcVer = {'HaImageProcVer': 0.5}
 Found_IDS = {}
 Buffer = {}
+EndBuffer = {}
 LDT_IDs = ["AB.TM.MRSS0697",
            "AB.TM.MRSS0698",
            "AB.TM.MRSS0699"]
@@ -87,16 +86,17 @@ class LDT_Properties(object):
         # Keep a running tally of the number of bytes written to file
         self.writtenLen += len(Data)
 
-    def completePacket(self):
-        # Once all parts of the packet have been received then finish
-        logger.info("End Packet Received")
-
+    def fileRename(self):
+        # Once all parts of the file have been received then finish
         # Check written equals expected and rename file
         if self.writtenLen == self.FILE_SIZE:
             logger.info("Packet Length as expected - renaming")
             newFile = self.write_file.with_suffix("")
             self.write_file.replace(newFile)
             self.write_file = newFile
+        else:
+            logger.error("Warning written length: %d not equal to FILE_SIZE %d ",
+                         self.writtenLen, self.FILE_SIZE)
 
     def moveHK(self):
         if self.DataType > 1:
@@ -209,8 +209,22 @@ def HaScan(ROV_DIR):
 
                 PKT_HD[0] = next(curFile)
 
-    if len(Buffer) > 0:
-        logger.error("Data remaining in buffer. Entries = %d", len(Buffer))
+    # Final buffer check
+    # Merge buffers
+    FinalBuf = {**Buffer, **EndBuffer}
+    if len(FinalBuf) > 0:
+        logger.info("Items remaining in buffers")
+        for item in FinalBuf:
+            logger.error(item)
+            if item[0] in Found_IDS:
+                Cur_LDT = Found_IDS.get(item[0])
+                checkBuffers(Cur_LDT)
+            else:
+                logger.error(
+                    "Initial LDT part not found for UnitID: %d", item[0])
+
+    if len(Buffer) + len(EndBuffer) > 0:
+        logger.info("Still %d items in buffer", len(Buffer)+len(EndBuffer))
     else:
         logger.info("LDT Buffer Empty")
     logger.info("Processing Rover .ha Files - Completed")
@@ -225,7 +239,8 @@ def HaPacketDecode(PKT_HD, PKT_ID, PKT_LINES, curFile, IMG_RAW_DIR):
     # First LDT Part
     if PKT_ID == LDT_IDs[0]:
         LDT_Cur_Pkt = LDT_Properties(PKT_Bin)
-        logger.info("New LDT part found with file ID: %s", LDT_Cur_Pkt.FILE_ID)
+        logger.info("New LDT part found with file ID: %s, and unitID %s",
+                    LDT_Cur_Pkt.FILE_ID, LDT_Cur_Pkt.Unit_ID)
         LDTHeader(PKT_Bin)
 
         # Check to see if ID already exists if not add to dict
@@ -242,99 +257,93 @@ def HaPacketDecode(PKT_HD, PKT_ID, PKT_LINES, curFile, IMG_RAW_DIR):
     elif PKT_ID == LDT_IDs[1]:
         IntP = LDT_Intermediate(PKT_Bin)
 
-        # Check to see if Unit ID already started if not add to dict
-        if IntP.Unit_ID in Found_IDS:
-            Cur_LDT = Found_IDS.get(IntP.Unit_ID)
-        else:
+        # Check to see if Unit ID already started if not add to buffer
+        if IntP.Unit_ID not in Found_IDS:
             logger.info("New Packet without first part - adding to buffer")
             LDTHeader(PKT_Bin)
-            Buffer.update({IntP.SEQ_No: PKT_Bin[20:-2]})
+            Buffer.update({(IntP.Unit_ID, IntP.SEQ_No): PKT_Bin[20:-2]})
 
-        # Check file should be written
-        if Cur_LDT.write:
-            # Verify SEQ number is as expected
-            Expected_SEQ = Cur_LDT.SEQ_No + 1
-            if Expected_SEQ == IntP.SEQ_No:
-                writeBytesToFile(Cur_LDT, PKT_Bin[20:-2])
-                Cur_LDT.SEQ_No += 1
-            else:
-                logger.warning("LDT parts not sequential")
-                logger.warning("Expected: %d", Expected_SEQ)
-                logger.warning("Got: %d", IntP.SEQ_No)
+        else:
+            Cur_LDT = Found_IDS.get(IntP.Unit_ID)
 
-                # First check if expected SEQ_No already exists in the buffer
-                if Expected_SEQ in Buffer:
-                    # Write to file the value in the buffer found
-                    writeBytesToFile(Cur_LDT, Buffer[Expected_SEQ])
-                    del Buffer[Expected_SEQ]
-                    logger.warning(
-                        "Wrote LDT part from buffer: %d", Expected_SEQ)
-                    Cur_LDT.SEQ_No += 1
-                    Expected_SEQ += 1
-
-                # Then check if the current packet is as expected
-                if Expected_SEQ == IntP.SEQ_No:
+            # Check file should be written
+            if Cur_LDT.write:
+                # Verify SEQ number is as expected
+                Expected_SEQ = (Cur_LDT.Unit_ID, Cur_LDT.SEQ_No + 1)
+                if Expected_SEQ == (IntP.Unit_ID, IntP.SEQ_No):
                     writeBytesToFile(Cur_LDT, PKT_Bin[20:-2])
-                    logger.warning(
-                        "Current LDT part now fits: %d", Expected_SEQ)
                     Cur_LDT.SEQ_No += 1
+                    Found_IDS.update({IntP.Unit_ID: Cur_LDT})
 
-                # If not then add to buffer
+                # Else add to buffer and try to rebuild
                 else:
-                    Buffer.update({IntP.SEQ_No: PKT_Bin[20:-2]})
-                    logger.warning("Added LDT part to buffer: %d", IntP.SEQ_No)
-
-        Found_IDS.update({IntP.Unit_ID: Cur_LDT})
+                    logger.warning("LDT parts not sequential")
+                    logger.warning("Expected: %d", Expected_SEQ[1])
+                    logger.warning("Got: %d", IntP.SEQ_No)
+                    Buffer.update(
+                        {(IntP.Unit_ID, IntP.SEQ_No): PKT_Bin[20:-2]})
+                    logger.warning(
+                        "Added LDT part to buffer: %d", IntP.SEQ_No)
+                    checkBuffers(Cur_LDT)
 
     elif PKT_ID == LDT_IDs[2]:
         # Check end of file and rename properly
         EndP = LDT_End(PKT_Bin)
+        logger.info("End Packet Received with unitID: %d", EndP.Unit_ID)
 
         # Check to see if Unit ID already started if not add to dict
-        if EndP.Unit_ID in Found_IDS:
-            Cur_LDT = Found_IDS.get(EndP.Unit_ID)
+        if EndP.Unit_ID not in Found_IDS:
+            logger.error("End Packet without first part - adding to EndBuffer")
+            EndBuffer.update({(EndP.Unit_ID, EndP.SEQ_No): PKT_Bin[20:-2]})
+
         else:
-            logger.error("End Packet without first part - adding to buffer")
-            Buffer.update({EndP.SEQ_No: PKT_Bin[20:-2]})
+            Cur_LDT = Found_IDS.get(EndP.Unit_ID)
 
-        # Check file should be written
-        if Cur_LDT.write:
-            # Verify SEQ number is as expected
-            Expected_SEQ = Cur_LDT.SEQ_No + 1
-            if Expected_SEQ == EndP.SEQ_No:
-                Cur_LDT.SEQ_No += 1
-                Cur_LDT.completePacket()
-                Cur_LDT.moveHK()
-                Found_IDS.pop(EndP.Unit_ID)
-                Cur_LDT.createJSON()
-
-            else:
-                logger.warning("END LDT not sequential")
-                logger.warning("Expected: %d", Expected_SEQ)
-                logger.warning("Got: %d", EndP.SEQ_No)
-
-                # First check if expected SEQ_No already exists in the buffer
-                if Expected_SEQ in Buffer:
-                    # Write to file the value in the buffer found
-                    writeBytesToFile(Cur_LDT, Buffer[Expected_SEQ])
-                    del Buffer[Expected_SEQ]
-                    logger.warning(
-                        "Wrote LDT part from buffer: %d", Expected_SEQ)
+            # Check file should be written
+            if Cur_LDT.write:
+                # Verify SEQ number is as expected
+                Expected_SEQ = (Cur_LDT.Unit_ID, Cur_LDT.SEQ_No + 1)
+                if Expected_SEQ == (EndP.Unit_ID, EndP.SEQ_No):
                     Cur_LDT.SEQ_No += 1
-                    Expected_SEQ += 1
+                    CompleteTelemetry(Cur_LDT)
+                    Found_IDS.update({EndP.Unit_ID: Cur_LDT})
 
-                # Then check if the current packet is as expected
-                if Expected_SEQ == EndP.SEQ_No:
-                    logger.warning("End LDT now fits: %d", Expected_SEQ)
-                    Cur_LDT.SEQ_No += 1
-                    Cur_LDT.completePacket()
-                    Buffer.pop(EndP.SEQ_No)
-
-                # If not then add to buffer
                 else:
-                    Buffer.update({Cur_LDT.SEQ_No: PKT_Bin[20:-2]})
+                    logger.warning("END LDT not sequential")
+                    logger.warning("Expected: %d", Expected_SEQ[1])
+                    logger.warning("Got: %d", EndP.SEQ_No)
+                    EndBuffer.update(
+                        {(EndP.Unit_ID, EndP.SEQ_No): PKT_Bin[20:-2]})
+                    logger.warning(
+                        "Added End part to buffer: %d, %d", EndP.Unit_ID, EndP.SEQ_No)
+                    checkBuffers(Cur_LDT)
 
-        Found_IDS.update({EndP.Unit_ID: Cur_LDT})
+
+def checkBuffers(Cur_LDT):
+    """Function checks the LDT part buffer and end buffer to add parts already found out of sequence"""
+
+    Expected_SEQ = (Cur_LDT.Unit_ID, Cur_LDT.SEQ_No + 1)
+
+    # First partial buffer
+    # Check if expected SEQ_No packets already exists in the buffer
+    while Expected_SEQ in Buffer:
+        # Write to file the value in the buffer found
+        writeBytesToFile(Cur_LDT, Buffer[Expected_SEQ])
+        Buffer.pop(Expected_SEQ)
+        logger.warning(
+            "Wrote LDT part from buffer: %d", Expected_SEQ[1])
+        Cur_LDT.SEQ_No += 1
+        Found_IDS.update({Cur_LDT.Unit_ID: Cur_LDT})
+        Expected_SEQ = (Expected_SEQ[0], Expected_SEQ[1] + 1)
+
+    # Then End Buffer
+    # Determine if the end packet has been found
+    if Expected_SEQ in EndBuffer:
+        logger.warning("End LDT now fits: %d", Expected_SEQ[1])
+        Cur_LDT.SEQ_No += 1
+        CompleteTelemetry(Cur_LDT)
+        EndBuffer.pop(Expected_SEQ)
+        Found_IDS.update({Cur_LDT.Unit_ID: Cur_LDT})
 
 
 def pkt_Len(PKT_HD):
@@ -360,7 +369,7 @@ def LDTHeader(PKT_Bin):
 
 
 def RestructureHK(ROV_DIR):
-    """Searches for .haHKNE and .haHKES generated from HaScan, produces well known pickle files"""
+    """Searches for .HKNE_raw and .HKES_raw generated from HaScan, produces the a single Unrpoc_HKTM pickle file"""
     logger.info("Processing any .ha HK that has been created")
 
     # Find Files
@@ -379,30 +388,12 @@ def RestructureHK(ROV_DIR):
             df = DataFrame(np.fromfile('curFile', dt))
 
 
-# def HaScan(ROV_DIR):
-#     """Searches for .ha Rover files and creates raw binary files
-#     for each image found"""
-#     logger.info("Processing Rover .ha Files")
-
-
-#     # Search through .ha files
-#     PKT_HD = [None] * 4
-
-#     for file in ROVER_HA:
-#         with open(file, 'r') as curFile:
-#             logger.info("Reading %s", file.name)
-#             # Read ha header and perform basic check
-#             HA_HEADER = [next(curFile) for x in range(5)]
-#             if HA_HEADER[4] != "<BEGIN_DATA_BLOCK>\n":
-#                 raise HaReadError("<BEGIN_DATA_BLOCK>: Line not found")
-
-#             # Read first packet header and determine packet type
-#             PKT_HD[0] = next(curFile)
-#             while PKT_HD[0] != "<END_DATA_BLOCK>\n":
-#                 PKT_HD[1:3] = [next(curFile) for x in range(3)]
-
-#                 if PKT_HD[3][0:8] != "<LENGTH>":
-#                     raise HaReadError("<LENGTH>: Line not found")
+def CompleteTelemetry(Cur_LDT):
+    """Once a telemetry has been fully reassembled, rename and create JSON"""
+    Cur_LDT.fileRename()
+    Cur_LDT.moveHK()
+    Cur_LDT.createJSON()
+    Found_IDS.pop(Cur_LDT.Unit_ID)
 
 
 if __name__ == "__main__":