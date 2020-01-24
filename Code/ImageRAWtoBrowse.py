4f2c629a05060f77641860a43b9de708b273e810
Chaldene <b.whiteside@ucl.ac.uk>
2020-01-24 17:51:08 +0000

    Dev notebook for generating png from NavCam PMG

--- Code/ImageRAWtoBrowse.py
+++ Code/ImageRAWtoBrowse.py
@@ -5,8 +5,10 @@ Created on Thu Nov 14 12:01:04 2019
 @author: ucasbwh
 """
 
-### Convert pci_raw to viewable .tiff and create appropriate label
+# Convert pci_raw to viewable 8-bit .png and create appropriate label
 
+import PC_Fns
+from decodeRAW_ImgHDR import decodeRAW_ImgHDR
 from pathlib import Path
 import numpy as np
 import imageio
@@ -14,35 +16,34 @@ import json
 import logging
 logger = logging.getLogger(__name__)
 
-from decodeRAW_ImgHDR import decodeRAW_ImgHDR
-import PC_Fns
 
 class ImgRawBrError(Exception):
     """error for unexpected things"""
     pass
 
+
 def Img_RAW_Browse(PROC_DIR):
 
     logger.info("Generating Image Browse Products from RAW Images")
 
-    ## Search for pci_raw files in the process directory
+    # Search for pci_raw files in the process directory
     RAW_FILES = PC_Fns.Find_Files(PROC_DIR, "IMG_RAW\*.pci_raw")
     if not RAW_FILES:
-        logger.error("No files found - ABORTING")  
+        logger.error("No files found - ABORTING")
 
-    ## Create for loop here
+    # Create for loop here
     for curFile in RAW_FILES:
         logger.info("Reading %s", curFile.name)
         with open(curFile, 'rb') as file:
-            img_rawheader = decodeRAW_ImgHDR(file.read(48))        
+            img_rawheader = decodeRAW_ImgHDR(file.read(48))
             raw_data = np.fromfile(file, dtype='>u2')
-            BrowseProps = {'RAW_Source':curFile.name}
+            BrowseProps = {'RAW_Source': curFile.name}
             BrowseProps.update({'PNG Bit-Depth': "8"})
 
-            ig = raw_data.reshape(1024,1024)
+            ig = raw_data.reshape(1024, 1024)
             img = ig >> 2
 
-            # Determine image rotation for preview            
+            # Determine image rotation for preview
             if img_rawheader['Cam'] == 1:
                 Br_img = np.rot90(img, k=3)
                 BrowseProps.update({'Browse_Rotation': '-90'})
@@ -50,17 +51,17 @@ def Img_RAW_Browse(PROC_DIR):
                 Br_img = np.rot90(img, k=1)
                 BrowseProps.update({'Browse_Rotation': '+90'})
             elif img_rawheader['Cam'] == 3:
-                Br_img = np.fliplr(img) 
+                Br_img = np.fliplr(img)
                 BrowseProps.update({'Browse_Transpose': "Left_Right"})
             else:
                 ImgRawBrError("Warning invalid CAM number")
-            
+
             # Create directory for Browse images
             BRW_DIR = PROC_DIR / "IMG_Browse"
             if not BRW_DIR.is_dir():
                 logger.info("Generating 'Processing' directory")
                 BRW_DIR.mkdir()
-                
+
             # Create 8-bit .png thumbnail
             write_filename = curFile.stem
             write_file = BRW_DIR / (write_filename + ".png")
@@ -70,7 +71,7 @@ def Img_RAW_Browse(PROC_DIR):
 
             imageio.imwrite(write_file, Br_img)
             logger.info("Creating .png: %s", write_file.stem)
-            
+
             # Read existing JSON file associated with RAW
             RAWJsonFile = curFile.with_suffix(".JSON")
             if not RAWJsonFile.exists():
@@ -80,22 +81,23 @@ def Img_RAW_Browse(PROC_DIR):
 
             # Append Browse Header information into dictionary for JSON file
             RAWJson.update({"Image Header RAW": img_rawheader})
-            RAWJson['Processing Info'].update({"Browse Properties": BrowseProps})     
+            RAWJson['Processing Info'].update(
+                {"Browse Properties": BrowseProps})
 
             write_file = BRW_DIR / (write_filename + ".json")
-            
+
             if write_file.exists():
                 write_file.unlink()
                 logger.info("Deleting file: %s", write_file.stem)
             with open(write_file, 'w') as f:
                 json.dump(RAWJson, f,  indent=4)
-    
+
     logger.info("Generating Image Browse Products from RAW Images Completed")
 
 
-    
 if __name__ == "__main__":
-    DIR= Path(input("Type the path to the folder where the PROC folder is located: "))
+    DIR = Path(
+        input("Type the path to the folder where the PROC folder is located: "))
 
     logging.basicConfig(filename=(DIR / 'processing.log'),
                         level=logging.INFO,
@@ -103,5 +105,5 @@ if __name__ == "__main__":
     logger.info('\n\n\n\n')
     logger.info("Running ImageRAWtoBrowse.py as main")
     logger.info("Reading directory: %s", DIR)
-    
+
     Img_RAW_Browse(DIR)
